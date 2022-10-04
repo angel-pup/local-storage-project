dd962e2bfd05dd42a10bdeac06f54e6166d28bf3
Spencer Stewart <r.spencer.stewart@gmail.com>
2022-10-03 21:08:06 -0500

    removed unused code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,6 @@
 let timerEl = document.querySelector('.timer');
 let startButton = document.querySelector('.start-button');
 
-function start() {
-    let secondsLeft = 10;
-
-    let timerInterval = setInterval(function() {
-       secondsLeft--;
-       timerEl.innerHTML = secondsLeft;
-
-       if(secondsLeft === 0) {
-           clearInterval(timerInterval);
-           loser();
-       }
-    }, 1000);
-}
-
 function loser() {
     timerEl.innerHTML = '10';
     alert('You lose!');
